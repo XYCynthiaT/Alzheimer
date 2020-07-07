23f9364a2deee9ea54ef71419811f136a76cd12c
XYCynthiaT <xytang1@hotmail.com>
2020-07-06 22:55:41 -0700

    delete pca.rda, save pca matrix.rda

--- app/pca.R
+++ app/pca.R
@@ -3,7 +3,33 @@ for (pkg in pkgs) {
         suppressPackageStartupMessages(library(pkg, character.only = T))
 }
 
-load("data/plots.rda")
+# pcaEnv = new.env(parent = .GlobalEnv)
+load("data/pca.rda")
+load("data/differentialExp.rda")
+
+volcanoPlot <- function(data){
+        # data are fit_xxx from differential expression analysis with linear model.
+        data$trend <- ifelse(
+                data$adj.P.Val>=0.05, 
+                'stable',
+                ifelse(
+                        data$logFC >= 0,
+                        'up',
+                        'down'
+                )
+        )
+        ggplot(data, aes(logFC, -log(adj.P.Val))) +
+        geom_point(aes(color = trend), alpha = 0.2) + 
+        geom_hline(yintercept = -log(0.05)) +
+        theme_bw()
+}
+
+pcaPlot <- function(pcaData, data) {
+        autoplot(pcaData, 
+                data = data, colour = 'diagnosis',
+                frame = TRUE, frame.type = 'norm')+
+        theme_bw()
+}
 
 pcaUI <- function(id) {
         ns <- NS(id)
@@ -36,20 +62,31 @@ pcaServer <- function(id, region) {
                 id,
                 function(input, output, session) {
                         output$volcano <- renderPlotly({
+                                # if(is.null(pcaEnv$vol_cbe)){
+                                #         print("loading plots.rda from volcano")
+                                #         load("data/plots.rda", envir = pcaEnv)
+                                #         print("done reading plots.rda from volcano")
+                                # }
                                 if (region() == "CBE") {
-                                        vol_cbe
+                                        volcanoPlot(fit_cbe)
                                 } 
                                 else {
-                                        vol_tcx
+                                        volcanoPlot(fit_tcx)
                                 }
                         })
-                        output$PCA <- renderPlotly(
+                        output$PCA <- renderPlotly({
+                                # if(is.null(pcaEnv$PCA_cbe)){
+                                #         print("pcaEnv is null")
+                                # } else {
+                                #         print("pcaEnv is not null")
+                                # }
+                                
                                 if (region() == "CBE") {
-                                        PCA_cbe
+                                        pcaPlot(pca_cbe, df_diag_cbe)
                                 } else {
-                                        PCA_tcx
+                                        pcaPlot(pca_tcx, df_diag_tcx)
                                 }
-                        )
+                        })
                 }
         )
 }
